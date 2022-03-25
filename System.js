7daa597db5ba921c813f6c4a23a67ddba0cf501d
Infinity847 <88909782+Infinity847@users.noreply.github.com>
2022-03-25 14:55:04 -0700

    Add files via upload

---
+++ System.js
@@ -0,0 +1,14 @@
+camera = document.getElementById("camera");
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+  });
+  function snapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id = "captured_image" src = "' + data_uri + '"/>';
+            });
+        
+  }
